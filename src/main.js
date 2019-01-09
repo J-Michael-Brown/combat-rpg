faa94d7d4960b7945653c15f5e8bca3ae009d527
Michael Brown and Kristin Brewer-Lowe <kristin@>
2019-01-09 11:48:57 -0800

    implement canvas w/player controls

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { weekdayCalculator } from './weekday-calculator';
+import { startGame } from './gameField';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
@@ -6,12 +6,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 $(document).ready(function() {
-  $('#date').submit(function(event){
-    event.preventDefault();
-    const month = parseInt($('input#month').val());
-    const day = parseInt($('input#day').val());
-    const year =parseInt($('input#year').val());
-    const dayOfWeek = weekdayCalculator(year, month, day);
-    $('#output').text(dayOfWeek);
-  });
+    startGame()
 });
